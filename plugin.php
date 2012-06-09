7217b55fe4216b1b8dcfb4824f14c63b47d85860
Daniel Chatfield <chatfielddaniel@googlemail.com>
2012-06-09 17:08:14 +0100

    Stable condition (it isn't erroring)

--- plugin.php
+++ plugin.php
@@ -14,26 +14,25 @@ include( dirname( __FILE__ ) ."/lava/lava.php" );
 
 class Volcanic_Pixels_Privacy_Plugin extends Lava_Plugin {
 
-	public $plugin_name = "Private Blog";
-	public $plugin_version = 4.05;
+	public $_plugin_name = "Private Blog";
+	public $_plugin_version = 4.05;
 
-	function register_settings() {
-		$this->settings()
-				->add_setting( 'enabled', 'checkbox' )
+	function _register_settings() {
+		$this->_settings()
+				->_add_setting( 'enabled', 'checkbox' )
 		;
 	}
 
-	function register_pages() {
-		$this->pages()
-				->add_settings_page()
-				->add_skins_page()
-					->set_page_title( $this->__( 'Login Page Skin' ) )
+	function _register_pages() {
+		$this->_pages()
+				->_add_settings_page()
+				->_add_skins_page()
+					->_set_page_title( $this->__( 'Login Page Skin' ) )
 		;
 	}
 }
 
 $the_plugin = new Volcanic_Pixels_Privacy_Plugin( __FILE__ );
 
-$the_plugin->register_action_methods( $the_plugin );
 
 ?>
